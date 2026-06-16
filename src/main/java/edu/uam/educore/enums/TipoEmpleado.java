e00aa8eeb1b9843bbfe322e2799313d28d4531e6
rdocr1995 <ricardo.alvarez1@uamcr.net>
2026-06-16 18:35:34 +0000

    Create TipoEmpleado.java

---
+++ src/main/java/edu/uam/educore/enums/TipoEmpleado.java
@@ -0,0 +1,15 @@
+
+package edu.uam.educore.enums;
+
+/**
+ *
+ * @author ralvarez
+ */
+public enum TipoEmpleado {
+    DOCENTE,
+    ADMINISTRATIVO,
+    GUARDA,
+    MISCELANEO,
+    MANTENIMIENTO
+ 
+}
